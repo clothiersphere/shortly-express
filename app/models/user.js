bd210b329b6c744c4df703312c0416b8f285db4e
spacemixup <xchrislee@gmail.com>
2015-06-08 17:29:16 -0700

    working on session authentication

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,20 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  initialize: function(){
+    this.on('creating', function(model, attrs, options){
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(model.get('password'), salt, null, function(err, hash) {
+          if (err) {
+            console.log(err);
+          } else {
+            model.set('password', hash);
+          }
+        });
+      });
+    });
+  }
 });
 
 module.exports = User;
